adf7e96b7e58e6ef4c32506f5d1138967116e850
Redouan Cherrat <recherra@c2r5p2.1337.ma>
2024-09-24 21:23:03 +0100

    Ultiamte fix: ambegious

--- parser/ambegious.c
+++ parser/ambegious.c
@@ -31,23 +31,26 @@ static int	check_n_files(char *str)
 	return (i);
 }
 
-static int	testing(char *str)
-{
-	int	len;
 
-	len = ft_strlen(str);
-	if (!len)
-		return (0);
-	if (str[len - 1] == '\x03')
-		if (check_n_files(str) != 1)
-			return (1);
-	return (0);
+static int prr(char *str)
+{
+    int i = 0;
+    while(str[i])
+    {
+        if (str[i] == '\x03')
+        {
+            if (check_n_files(str) != 1)
+                return 1;
+        }
+        i++;
+    }
+    return 0;
 }
 
 int	check_ambg(char *str, t_env *envs)
 {
-	if (testing(str))
-		return (1);
+    if (prr(str))
+        return 1;
 	if (check_in_env(str, envs))
 	{
 		if (!ft_strcmp(str, "\x03") || check_n_files(str) != 1)
